ca23e07b372552f3f50b26deefbb49934a23c283
swe2698 <b.gokul100@gmail.com>
2020-03-02 11:30:13 +0530

    updated

--- Mongodb-workfiles/demo_create_mongo_db.js
+++ Mongodb-workfiles/demo_create_mongo_db.js
@@ -1,5 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+var url = "mongodb+srv://Gokulb1998:evz54180827@mydb98-jvqcp.mongodb.net/test?retryWrites=true&w=majority";
 
 MongoClient.connect(url,{useUnifiedTopology: true},function(err, db) {
   if (err) throw err;
